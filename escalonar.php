c47c6a71c0ba3aa2b7f3c778dccf344a7cc1f383
Alexandre Werneck <xanwerneck@gmail.com>
2014-03-31 22:11:45 -0300

    modificacao nas operacoes elementares

--- escalonar.php
+++ escalonar.php
@@ -13,26 +13,26 @@ if($_POST){
 		}
 	}
 
+	$first_mat = $elementos;
+	$first_mat_l = $linhas;
+	$first_mat_c = $colunas;
+
 	$tmp = EliminaLinhasNulas($elementos,$linhas,$colunas);
 	$elementos = $tmp[1];
 	$linhas    = $tmp[0];
 	
-	while ($k < $linhas) {
+	$elementos = TrocaLinhas($elementos, $linhas, $colunas);
 
-		$elementos = TrocaLinhas($elementos, $linhas, $colunas);
+	$elementos = Operacoes($elementos,$colunas,$linhas);
 
-		$elementos = Operacoes($elementos,$colunas,$k);
+	$tmp       = EliminaLinhasNulas($elementos,$linhas,$colunas);
+	$elementos = $tmp[1];
+	$linhas    = $tmp[0];
 
-		$tmp       = EliminaLinhasNulas($elementos,$linhas,$colunas);
-		$elementos = $tmp[1];
-		$linhas    = $tmp[0];
+	$elementos = ArrumaFracoes($elementos,$linhas,$colunas);
 
-
-		$k++;
-	}
-
-	echo '<pre>';
-	var_dump($elementos);
+	//echo '<pre>';
+	//var_dump($elementos);
 
 }
 
@@ -66,19 +66,30 @@ function TrocaLinhas($elementos, $linhas, $colunas){
 
 }
 
-function Operacoes($elementos,$colunas,$k){
+function Operacoes($elementos,$colunas,$linhas){
 
-	$operador = 0;
-	for ($j=1; $j <= $colunas; $j++) { 
-		if($elementos[$k+1][$j]!=0 && $operador == 0){
-			$operador = $j;
-			$cont = $elementos[$k+1][$operador];
+	$k = 1;
+	while($k < $linhas){
+
+		for ($i=$k; $i <= $linhas ; $i++) { 
+			$operador = 0;
+			for ($j=1; $j <= $colunas ; $j++) { 			
+				if($elementos[$i+1][$j]!=0 && $operador == 0){
+					$operador = $j;
+					$cont = $elementos[$i+1][$operador];
+					$w    = $cont / $elementos[$k][$j];
+				}
+				if($operador != 0){
+					$tmp = $elementos[$k][$j] * $w;					
+					$elementos[$i+1][$j] = $elementos[$i+1][$j] - $tmp;
+				}
+			}
 		}
-		if($operador != 0){
-			$tmp = $elementos[$k][$j] * $cont;
-			$elementos[$k+1][$j] = $elementos[$k+1][$j] - $tmp;
-		}
+
+
+		$k++;
 	}
+
 	return $elementos;
 }
 
@@ -111,6 +122,64 @@ function reorgArray($elem){
 		$ret[$i+1] = $elem[$i];
 	}
 	return $ret;
+}
+
+function ArrumaFracoes($elementos,$linhas,$colunas){
+		for ($i=1; $i <= $linhas ; $i++) { 
+			for ($j=1; $j <= $colunas; $j++) { 
+				if(is_float($elementos[$i][$j])){
+					$tmp = dec2fracso($elementos[$i][$j]);
+					if($tmp[0] == "0"){
+    					$elementos[$i][$j] = 0;
+    				}else{
+    					$elementos[$i][$j] = $tmp[0].'/'.$tmp[1];
+    				}
+				}
+			}
+		}
+		return $elementos;
+}
+
+function dec2fracso($dec){
+    //Negative number flag.
+    $num=$dec;
+    if($num<0){
+        $neg=true;
+    }else{
+        $neg=false;
+    }
+
+    //Extracts 2 strings from input number
+    $decarr=explode('.',(string)$dec);
+
+    //Checks for divided by zero input.
+    if($decarr[1]==0){
+        $decarr[1]=1;
+        $fraccion[0]=$decarr[0];
+        $fraccion[1]=$decarr[1];
+        return $fraccion;
+    }
+
+    //Calculates the divisor before simplification.
+    $long=strlen($decarr[1]);
+    $div="1";
+    for($x=0;$x<$long;$x++){
+        $div.="0";
+    }
+
+    //Gets the greatest common divisor.
+    $x=(int)$decarr[1];
+    $y=(int)$div;
+    $gcd=gmp_strval(gmp_gcd($x,$y));
+
+    //Calculates the result and fills the array with the correct sign.
+    if($neg){
+        $fraccion[0]=((abs($decarr[0])*($y/$gcd))+($x/$gcd))*(-1);
+    }else{
+        $fraccion[0]=(abs($decarr[0])*($y/$gcd))+($x/$gcd);
+    }
+    $fraccion[1]=($y/$gcd);
+    return $fraccion;
 }
 
 ?>
@@ -146,7 +215,9 @@ function reorgArray($elem){
 			<form action="escalonar.php" method="post">
 			  <fieldset>
 			    <legend>Cálculo da Matriz Escalonada</legend>
-			    
+			    <?
+
+				if(!$_POST){ ?>
 			    <div class="row-fluid">
 					<input class="span1" name="col1_1">
 					<input class="span1" name="col1_2">
@@ -165,6 +236,30 @@ function reorgArray($elem){
 					<input class="span1" name="col3_3">
 				</div>
 				<br>
+				<? }else{
+
+					for ($i=1; $i <= $first_mat_l; $i++) { 
+						 echo '<div class="row-fluid">';
+						for ($j=1; $j <= $first_mat_c; $j++) { 
+							echo '<input class="span1" name="colN'.$i.'_'.$j.'" value='.$first_mat[$i][$j].'>';
+						}
+						echo '</div><br>';
+					}
+
+					echo '<hr/>';
+
+					for ($i=1; $i <= $linhas; $i++) { 
+						 echo '<div class="row-fluid">';
+						for ($j=1; $j <= $colunas; $j++) { 
+							echo '<input class="span1" name="colN'.$i.'_'.$j.'" value='.$elementos[$i][$j].'>';
+						}
+						echo '</div><br>';
+					}
+
+					
+				} 
+				 ?>
+
 				<input type="hidden" id="linhas" name="linhas" value="3">
 				<input type="hidden" id="colunas" name="colunas" value="3">
 			    <button type="submit" class="btn">Submit</button>
